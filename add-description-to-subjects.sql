fce34cc64d6c6a54213a823d2ed5e6ff10fca769
maarten <maarten1096@gmail.com>
2026-02-04 19:48:39 +0100

    auto commit

---
+++ add-description-to-subjects.sql
@@ -0,0 +1,12 @@
+-- Add description field to subjects table if it doesn't exist
+ALTER TABLE public.subjects
+ADD COLUMN IF NOT EXISTS description text;
+
+-- Force PostgREST to reload schema to pick up the new column
+SELECT pg_notify('pgrst', 'reload schema');
+
+-- Verify the change
+SELECT column_name, data_type, is_nullable
+FROM information_schema.columns
+WHERE table_name = 'subjects' AND table_schema = 'public'
+ORDER BY ordinal_position;
