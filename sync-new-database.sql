c83ee0704eb42c3ec2eec0ee80820b74e6450a8e
maarten <maarten1096@gmail.com>
2026-02-07 14:56:54 +0100

    auto commit

--- sync-new-database.sql
+++ sync-new-database.sql
@@ -395,7 +395,7 @@ SET search_path = public
 AS $$
   SELECT EXISTS (
     SELECT 1 FROM public.user_roles
-    WHERE user_id = _user_id AND role = _role
+    WHERE user_id = _user_id AND role::text = _role
   )
 $$;
 
