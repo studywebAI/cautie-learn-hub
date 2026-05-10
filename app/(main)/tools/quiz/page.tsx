c81734857b0bbcaedaa03b3785e96368e6463b19
maarten <maarten1096@gmail.com>
2026-05-10 19:45:01 +0200

    auto commit

--- app/(main)/tools/quiz/page.tsx
+++ app/(main)/tools/quiz/page.tsx
@@ -536,7 +536,7 @@ function QuizPageContent() {
 
   return (
     <WorkbenchShell title="Quiz" sidebar={sidebar} breadcrumbIcon={<BrainCircuit className="h-4 w-4" />}>
-      <div className="flex h-full w-full flex-col pl-3 pr-2">
+      <div className="flex h-full w-full flex-col pl-0">
         <SourceInput
           toolId="quiz"
           value={sourceText}
